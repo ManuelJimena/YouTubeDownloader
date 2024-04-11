36a64a2a16e577f8e96c27173f691d94cea2c024
Manuel Jimena García <Manuel.jimena29@gmail.com>
2024-04-11 10:57:14 +0200

    refact briefing

--- src/utils/MapFetchs.js
+++ src/utils/MapFetchs.js
@@ -1,10 +1,21 @@
 const MapYoutube = (video) => {
+  const sortedFormats = video.formats.sort((a, b) => {
+
+    const qualityA = parseInt(a.qualityLabel.replace('p', ''), 10);
+    const qualityB = parseInt(b.qualityLabel.replace('p', ''), 10);
+
+    return qualityB - qualityA;
+  });
+
+  const highestQualityFormat = sortedFormats[0];
+
   const youtubevideo = {
     id: video.id,
     thumb: video.thumbnail,
-    video: video.formats.reverse(),
+    video: [highestQualityFormat],
     title: video.title,
   };
+
   return youtubevideo;
 };
 
