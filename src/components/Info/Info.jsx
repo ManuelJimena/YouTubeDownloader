7f69400e6eff27625f309b648ef0748a49526486
Manuel Jimena García <Manuel.jimena29@gmail.com>
2024-04-12 15:01:16 +0200

    refact audio

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -13,7 +13,7 @@ const Info = () => {
         </figure>
         <div className="text_info">
           <h3>Características de YouTubeDownloader</h3>
-            <p><b>YouTubeDownloader</b> permite descargar <b>vídeos</b> en formato <b>.mp4</b> y extraer <b>audio</b> en formato <b>.mp3</b>. Las APIs que utiliza son gratuitas, por tanto la calidad de los vídeos descargados está limitada a un máximo de <b>720p</b> y tiene un <b>número limitado de descargas diarias</b>, que se restablecen automáticamente al día siguiente.</p>
+            <p><b>YouTubeDownloader</b> permite descargar <b>vídeos</b> en formato <b>.mp4</b> y extraer <b>audio</b> en formato <b>.mp3</b>. Las APIs que utiliza son gratuitas, por tanto la calidad máxima de los vídeos está limitada a <b>720p</b> y tiene un <b>número limitado de descargas diarias</b>, que se restablecen automáticamente al día siguiente.</p>
             <p>La conversión de vídeo a formato .mp3 puede requerir un tiempo considerable, especialmente cuando se trata de vídeos más largos. Esta espera se justifica por la transformación necesaria para extraer audio de calidad a partir del vídeo.</p>
         </div>
       </div>
